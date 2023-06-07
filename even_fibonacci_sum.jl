97bb661c0593c90f40bf58615ea699d15009a325
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-07 19:42:22 -0400

    Fixes to documentation

--- even_fibonacci_sum.jl
+++ even_fibonacci_sum.jl
@@ -9,9 +9,22 @@ Find the sum of the even Fibonacci numbers up to (but not including) 4_000_000
 """
     rfibonacci(n)
 
-Recursive method to calculate fibonacci numbers. Works well (and its pretty) for
-smaller values. Need to either alter the stack size or create an iterative
-method to compute large values of Fibonacci numbers.
+# Description
+Recursive method to calculate fibonacci numbers.
+
+# Example
+```jldoctest
+julia> rfibonacci(25)
+46368
+```
+
+# Warning
+Works well for smaller values. Unknown behavior when values get too high.
+Values that are too large will cause integer overflow (looks like integer
+wrapping) and quite possibly stack overflow errors.
+
+Need to either alter the stack size or create an iterative method to compute
+large values of Fibonacci numbers.
 """
 function rfibonacci(n)
     if n <= 0
@@ -19,17 +32,30 @@ function rfibonacci(n)
     elseif n == 1
         return 1
     else
-        return fibonacci(n-1) + fibonacci(n-2)
+        return rfibonacci(n-1) + rfibonacci(n-2)
     end
 end
+
 
 """
     fibonacci(n)
 
-Iterative method to calculate fibonacci numbers. Able to calculate very large
-numbers. Better way to do this would be to store already calculated values in a
-hashmap so don't have to spend time recalculating them (use some kind of LRU
-algorithm).
+# Description
+Iterative method to calculate fibonacci numbers.
+
+# Example
+```jldoctest
+julia> fibonacci(10_000)
+2788724563990792802
+```
+
+# Warning
+Able to calculate very large numbers. Better way to do this would be to store
+already calculated values in a hashmap so don't have to spend time recalculating
+them (use some kind of LRU algorithm).
+
+Significantly large numbers will need a different type to prevent the integer
+overflow from occurring.
 """
 function fibonacci(n)
     if n <= 0
