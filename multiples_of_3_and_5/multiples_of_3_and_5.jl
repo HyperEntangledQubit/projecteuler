6c92e052a55e065de51a17bfdbf13c8e1b08f1cc
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-06 22:33:39 -0400

    Adding problem statement to multiples of 3 and 5

--- multiples_of_3_and_5/multiples_of_3_and_5.jl
+++ multiples_of_3_and_5/multiples_of_3_and_5.jl
@@ -1,5 +1,12 @@
 #!/usr/bin/env julia
 
+#= Problem Statement
+
+Find the sum of all natural numbers up to (but not including) 1000 that are
+multiples of 3 or 5.
+
+=#
+
 result = 0
 for i in 1:999 # Ranges in Julia are inclusive for the ending delimiter
     if i % 3 == 0 || i % 5 == 0
