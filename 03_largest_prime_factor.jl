8911da194bd24f17b7a6ac5578e7ff913bf218b8
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-13 23:15:14 -0400

    Solution works now need to refactor

--- 03_largest_prime_factor.jl
+++ 03_largest_prime_factor.jl
@@ -1,7 +1,5 @@
 #!/usr/bin/env julia
 
-using Primes
-
 #= Problem Statement
 #
 
@@ -10,7 +8,7 @@ What is the largest prime factor of 60085147543?
 =#
 
 """
-    isPrime(n)
+    is_prime(n)
 
 # Description
 Method to check if a given number is prime
@@ -27,14 +25,23 @@ function is_prime(n)
         return false
     end
 
-    upper_bound = sqrt(n)
-    for i in 2:upper_bound
+    for i in 2:sqrt(n)
         if n % i == 0
             return false
         end
     end
 
     return true
+end
+
+"""
+"""
+function gcd(x, y)
+    if x == 0
+        return y
+    end
+
+    return gcd(y % x, x)
 end
 
 """
@@ -54,15 +61,19 @@ This method is slow... since we are using rather large numbers here.
 TODO Redo this method using multithreading
 """
 function largest_prime_factor(n)
-    largest_prime = 0
+    prime_factors = Int64[]
     # Only need to check up to the sqrt of n
-    for i in reverse(1:trunc(Int64, sqrt(n)))
+    for i in 1:trunc(Int64, sqrt(n))
         if is_prime(i)
-            println(i)
+            ggcd = gcd(i, n)
+            if ggcd != 1
+                # println(ggcd)
+                push!(prime_factors, ggcd)
+            end
         end
     end
 
-    return largest_prime
+    return maximum(prime_factors)
 end
 
 res = largest_prime_factor(600851475143)
