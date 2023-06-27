664323f03d2acb0fb6d2da2ec1b44da7fbec49fc
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-26 20:10:29 -0400

    Fixed the logic to determine if palindrome

--- 04_largest_palindrome_product.jl
+++ 04_largest_palindrome_product.jl
@@ -4,3 +4,28 @@
 
 Find the largest palindromic number made from the product of two 3-digit numbers
 =#
+
+"""
+    is_palindrome(n)
+
+# Description
+Returns a bool if n is a palindromic number
+"""
+function is_palindrome(n)
+    passed_in = string(n)
+
+    # String should read the same way when reversed
+    if passed_in == reverse(passed_in)
+        return true
+    end
+
+    return false
+end
+
+for i in 1:999
+    result = is_palindrome(i)
+    if result
+        println("$i is a palindrome: $result")
+    end
+end
+
